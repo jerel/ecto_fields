c0b7bbec12a20cb79fd1949486097eea40a0c23e
Jerel Unruh <mail@unruhdesigns.com>
2016-06-27 16:13:42 -0500

    Release v0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoFields.Mixfile do
 
   def project do
     [app: :ecto_fields,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
