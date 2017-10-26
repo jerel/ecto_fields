b11a4554bc84b983e15de678eee72fb23a80fe96
Jerel Unruh <jerel.unruh@toyotaconnected.com>
2017-10-26 10:19:53 -0500

    Release v1.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoFields.Mixfile do
 
   def project() do
     [app: :ecto_fields,
-     version: "1.1.1",
+     version: "1.1.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
