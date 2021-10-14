c9c9ba2d999c4aab6482130bbe40a3b4758a6d07
James King <t.james.king@gmail.com>
2021-10-13 21:17:44 -0700

    Use strict address parsing in EctoFields.IP.cast()
    Both EctoFields.IPv4 and EctoFields.IPv6 use strict address parsing in
    their cast, update EctoFields.IP to match.  Prevents validation of plain
    integers and other obscure short-form IPv4 addresses.

--- lib/fields/ip.ex
+++ lib/fields/ip.ex
@@ -18,7 +18,7 @@ defmodule EctoFields.IP do
       :error
   """
   def cast(ip) when is_binary(ip) and byte_size(ip) > 0 do
-    case ip |> String.to_charlist() |> :inet_parse.address() do
+    case ip |> String.to_charlist() |> :inet.parse_strict_address() do
       {:ok, _} -> {:ok, ip}
       {:error, _} -> :error
     end
